a06d55dbe19159298ea05c68a823c84944729a2e
sachinmadhwa <kjhasachin@gmail.com>
2022-04-10 17:20:14 +0545

    databse creat done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/home', function () {
+   echo "Home";
+});
 
 Route::get('/about', function () {
+    return view("about");
     
-    echo "This is About page";
+});
+Route::get('/contact', function () {
+    
+   return view("contact");
+});
+
+Route::get('/contact', [ContactController::class, 'index']);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
